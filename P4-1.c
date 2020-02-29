9f41dcda242b680146363ff7ad18fac317e4286a
AlfredoGuadalupe <54385258+AlfredoGuadalupe@users.noreply.github.com>
2020-02-29 13:15:15 -0600

    Update P4-1.c

--- P4-1.c
+++ P4-1.c
@@ -3,10 +3,17 @@
 
 int main (){
 	int *arreglo, num, cont;
+	//pide al usuario el numero de elementos del conjunto y guarda el dato en una variable 
 	printf("¿Cuantos elementos tiene el conjunto?\n");
 	scanf("%d",&num);
+	/*
+	Se reserva memoria de acuerdo a lo que el usuario indique que sea el tamaño del arreglo, se tiene que especificar
+	de que tipo serán los datos que se reserven
+	*/
 	arreglo = (int *)malloc (num * sizeof(int));
 	if (arreglo!=NULL) {
+		/*Mientras tenga datos almacenados se imprime el contenido del arreglo separados por una tabulacion
+		ademas de estar encerrado en corchetes*/
 		printf("Vector reservado:\n\t["); 
 		for (cont=0 ; cont<num ; cont++){
 			printf("\t%d",*(arreglo+cont));
