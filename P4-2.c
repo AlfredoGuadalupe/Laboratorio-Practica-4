92af0689e938c757568fe76ff6ef3a930b73e121
AlfredoGuadalupe <54385258+AlfredoGuadalupe@users.noreply.github.com>
2020-02-29 13:18:22 -0600

    Update P4-2.c

--- P4-2.c
+++ P4-2.c
@@ -3,16 +3,24 @@
 
 int main (){
 	int *arreglo, num, cont;
+	//pide al usuario el numero de elementos del conjunto y guarda el dato en una variable
 	printf("¿Cuantos elementos tiene el conjunto?\n");
 	scanf("%d",&num);
+	/*
+	Se reserva memoria de acuerdo a lo que el usuario indique que sea el tamaño del arreglo y sustituye los 
+	valores de esos espacios de memoria por 0, se tiene que especificar de que tipo serán los datos que se reserven
+	*/
 	arreglo = (int *)calloc (num, sizeof(int));
 	if (arreglo!=NULL) {
+		/*Mientras tenga datos almacenados se imprime el contenido del arreglo segarados por una tabulacion
+		ademas de estar encerrado en corchetes*/
 		printf("Vector reservado:\n\t["); 
 		for (cont=0 ; cont<num ; cont++){
 			printf("\t%d",*(arreglo+cont));
 		}
 		printf("\t]\n");
 		printf("Se libera el espacio reservado.\n");
+		//El comando free libera la memoria reservada para permitir que otro programa pueda usarla
 		free(arreglo);
 	}
 	return 0;
